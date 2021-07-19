9353c9d540a5335193cbd99031f0cccd8723b9dd
곽온겸 <dhsrua2015@gmail.com>
2021-07-20 00:46:26 +0900

    정렬 심화 문제

--- test.py
+++ test.py
@@ -1,31 +1,5 @@
-# BOJ 2750 수 정렬하기
-
-# 퀵 정렬로 풀기
-arr = list(map(int, input().split()))
-
-def QuickSort(arr, start, end):
-    if start >= end:
-        return
-    
-    key = start
-    i = start + 1
-    j = end
-
-    while i <= j:
-        while i < (end + 1) and arr[i] <= arr[key]:
-            i += 1
-        while j > start and arr[j] >= arr[key]:
-            j -= 1
-
-        if i > j:
-            arr[key], arr[j] = arr[j], arr[key]
-        else:
-            arr[i], arr[j] = arr[j], arr[i]
-
-        
-    QuickSort(arr, start, j - 1)
-    QuickSort(arr, j + 1, end)
-
-QuickSort(arr, 0, len(arr) - 1)
-
-print(*arr)
+a, b = input().split()
+if a > b:
+    print('yes')
+else:
+    print('no')
