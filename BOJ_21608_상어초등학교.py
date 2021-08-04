03d8a48b8448eae224cb542e221238c6d7d53f2c
gwak-ongyeom <dhsrua2015@gmail.com>
2021-08-05 00:37:24 +0900

    BOJ 21608 상어 초등학교

--- BOJ_21608_상어초등학교.py
+++ BOJ_21608_상어초등학교.py
@@ -19,10 +19,10 @@
 
 자리배치가 완료된 뒤 만족도 조사
 인접학생 0 => 0
-        1 => 1
-        2 => 10
-        3 => 100
-        4 => 1000
+    1 => 1
+    2 => 10
+    3 => 100
+    4 => 1000
 
 400 * 4(주변 탐색) * log400 + 400 * 4 
 N^2 * 4 * 2logN = 8 N^2 logN => N < 20이므로 괜춘
@@ -33,6 +33,69 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-info = [list(map(int, input().split())) for _ in range(N**2)]
+info = {}
+for n in range(N*N):
+    line = list(map(int, input().split()))
+    info[line[0]] = line[1:]
+# 델타 탐색: 상, 하, 좌, 우
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+# 반 배치도
+class_info = [[0] * N for i in range(N)]
 
+# 탐색
+for key, value in info.items():
+    student = int(key)
+    friends = value
+    # 위치 정보(인접, 빈칸, 위치)
+    best_loc = [-1, -1, 0, 0]
+    best_fr = []
+    for i in range(N):
+        for j in range(N):
+            if class_info[i][j]:
+                continue
+            y, x = i, j
+            loc = [0, 0, i, j]
+            fr = []
+            # 델타 탐색
+            for d in range(4):
+                y += dy[d]
+                x += dx[d]
+                if 0 <= y < N and 0 <= x < N:
+                    if class_info[y][x] == 0:
+                        loc[1] += 1
+                    else:
+                        if class_info[y][x] in friends:
+                            loc[0] += 1
+                y -= dy[d]
+                x -= dx[d]
+            # 먼저 들어온 값보다 커야만 대체 가능
+            if loc[0] > best_loc[0]:
+                best_loc = loc
+            elif loc[0] == best_loc[0]:
+                if loc[1] > best_loc[1]:
+                    best_loc = loc
+    class_info[best_loc[2]][best_loc[3]] = student
+
+score = 0
+for i in range(N):
+    for j in range(N):
+        y, x = i, j
+        point = 0
+        for d in range(4):
+            y += dy[d]
+            x += dx[d]
+            if 0 <= y < N and 0 <= x < N:
+                if class_info[y][x] in info[class_info[i][j]]:
+                    point += 1
+            y -= dy[d]
+            x -= dx[d]
+        if point:
+            score += 10**(point-1)
+        
+print(score)
+
+
+            
+            
 
