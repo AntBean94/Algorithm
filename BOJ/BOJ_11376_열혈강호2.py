ba7f931d6a09f54c3857eac599854842042d2fb1
gwak-ongyeom <dhsrua2015@gmail.com>
2021-08-17 00:23:03 +0900

    BOJ 11376 열혈강호2

---
+++ BOJ/BOJ_11376_열혈강호2.py
@@ -0,0 +1,31 @@
+# BOJ 11376 열혈강호 2
+
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [[0]]
+for i in range(N):
+    info = list(map(int, input().split()))
+    graph.append(info[1:])
+
+task = [0] * (M + 1)
+
+def bipartite(x):
+    for y in graph[x]:
+        if visit[y]:
+            continue
+        visit[y] = True
+        if task[y] == 0 or bipartite(task[y]):
+            task[y] = x
+            return True
+    return False
+
+ans = 0
+for j in range(2):
+    for i in range(1, N + 1):
+        visit = [False] * (M + 1)
+        if bipartite(i):
+            ans += 1
+print(ans)
