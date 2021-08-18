82108fde98dfd63fdbf4e9f0cb5a1afdcf6adb57
gwak-ongyeom <dhsrua2015@gmail.com>
2021-08-18 23:37:10 +0900

    BOJ 1463 1로 만들기

---
+++ BOJ/BOJ_1463_1로만들기.py
@@ -0,0 +1,26 @@
+# BOJ 1463 1로 만들기
+
+N = int(input())
+
+arr = [0] * (N + 1)
+for i in range(N, 1, -1):
+    # 3으로 나누기
+    if i % 3 == 0:
+        # 최솟값 여부 확인
+        if arr[i // 3] == 0:
+            arr[i // 3] = arr[i] + 1
+        else:
+            if arr[i] + 1 < arr[i // 3]:
+                arr[i // 3] = arr[i] + 1
+    if i % 2 == 0:
+        if arr[i // 2] == 0:
+            arr[i // 2] = arr[i] + 1
+        else:
+            if arr[i] + 1 < arr[i // 2]:
+                arr[i // 2] = arr[i] + 1
+    if arr[i - 1] == 0:
+        arr[i - 1] = arr[i] + 1
+    else:
+        if arr[i] + 1 < arr[i - 1]:
+            arr[i - 1] = arr[i] + 1
+print(arr[1])
