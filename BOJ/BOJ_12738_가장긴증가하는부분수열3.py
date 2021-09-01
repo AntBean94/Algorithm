43f3d37facee9a3d2b84e3d40621bae7d124e13e
gwak-ongyeom <dhsrua2015@gmail.com>
2021-09-01 21:02:17 +0900

    BOJ 12738 가장 긴 증가하는 부분 수열 3(이분탐색)

---
+++ BOJ/BOJ_12738_가장긴증가하는부분수열3.py
@@ -0,0 +1,17 @@
+# BOJ 12738 가장 긴 증가하는 부분 수열 3
+
+import sys
+import bisect
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+table = [-1000000001]
+for i in arr:
+    if i > table[-1]:
+        table.append(i)
+    else:
+        table[bisect.bisect_left(table, i)] = i
+
+print(len(table) - 1)
