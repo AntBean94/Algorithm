ac484cc2f91df0fcef5648f55f60a91b50e84267
gwak-ongyeom <dhsrua2015@gmail.com>
2021-09-03 22:30:46 +0900

    BOJ 1504 특정한 최단 경로(dijkstra)

--- BOJ/BOJ_1504_특정한최단경로.py
+++ BOJ/BOJ_1504_특정한최단경로.py
@@ -1,8 +1,5 @@
 # BOJ 1504 특정한 최단 경로
 
-import heapq
-import sys
-input = sys.stdin.readline
 
 '''
 1, A, B, N
@@ -13,14 +10,17 @@ input = sys.stdin.readline
 => A, B의 최소거리를 구한다.
 
 각 최소거리를 조합해 1부터 N정점까지의 최소거리를 구한다.
-
 '''
+
+import heapq
+import sys
+input = sys.stdin.readline
 
 N, E = map(int, input().split())
 INF = 1000000000
 graph = [[] for _ in range(N + 1)]
-distance = [[INF] * (N + 1) for _ in range(4)]
-print(distance)
+distance = [[INF] * (N + 1) for _ in range(3)]
+# print(distance)
 for i in range(E):
     a, b, d = map(int, input().split())
     graph[a].append([b, d])
@@ -34,7 +34,7 @@ def dijkstra(start, k):
     while q:
         dist, now = heapq.heappop(q)
         if distance[k][now] < dist: continue
-        for di, nxt in graph[now]:
+        for nxt, di in graph[now]:
             cost = dist + di
             if cost < distance[k][nxt]:
                 distance[k][nxt] = cost
@@ -44,6 +44,13 @@ arr = [1, A, N]
 for i in range(3):
     start = arr[i]
     dijkstra(start, i)
-
 for d in distance:
-    print(d)
+    print(d[1:])
+# 정답 출력
+# 1, 3, 2, 4 vs 1, 2, 3, 4
+ans_A = distance[0][A] + distance[1][B] + distance[2][B]
+ans_B = distance[0][B] + distance[1][B] + distance[2][A]
+if min(ans_A, ans_B) >= 1000000000:
+    print(-1)
+else:
+    print(min(ans_A, ans_B))
