138b5589edea431a2ef4d0196b5ae216b87cc7d8
gwak-ongyeom <dhsrua2015@gmail.com>
2021-09-06 22:36:18 +0900

    Programmers 카카오기출 카드 짝 맞추기

---
+++ Programmers/PG_카드짝맞추기.py
@@ -0,0 +1,28 @@
+# Programmers 카드 짝 맞추기
+
+''' 접근 방법
+그리디 알고리즘?
+
+
+
+'''
+
+
+def solution(board, r, c):
+    answer = 0
+    
+
+
+
+    return answer
+
+
+
+
+
+test_case = [
+    [[[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]], 1, 0],
+    [[[3,0,0,2],[0,0,1,0],[0,1,0,0],[2,0,0,3]], 0, 1]
+]
+for tc in test_case:
+    print(solution(tc[0], tc[1], tc[2]))
