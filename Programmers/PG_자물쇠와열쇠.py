9a532aec442620705687c7b874612ed2bde00d1e
gwak-ongyeom <dhsrua2015@gmail.com>
2021-09-11 19:06:19 +0900

    Programmers 카카오 자물쇠와 열쇠(푸는중)

---
+++ Programmers/PG_자물쇠와열쇠.py
@@ -0,0 +1,22 @@
+# Programmers 자물쇠와 열쇠
+
+'''
+자물쇠에 빈칸이 없어야 열 수 있다.
+
+
+
+
+'''
+
+
+
+def solution(key, lock):
+    answer = True
+    return answer
+
+
+
+
+test_case = [
+    [[[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]]
+]
