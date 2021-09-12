c86aa5b6d0144c879318de76e55dcde98ceb0e9d
gwak-ongyeom <dhsrua2015@gmail.com>
2021-09-12 14:33:45 +0900

    BOJ 2447 별 찍기-10(재귀)

--- BOJ/BOJ_2447_별찍기10.py
+++ BOJ/BOJ_2447_별찍기10.py
@@ -1,10 +1,24 @@
 # BOJ 2447 별 찍기 - 10
+import sys
 
 N = int(input())
+board = [[' '] * N for _ in range(N)]
 
-def star(n):
-    for i in range(n):
-        print('*', end='')
-star(N)
+def star(n, y, x):
+    # 탈출 조건
+    if n == 1:
+        board[y][x] = '*'
+        return
 
+    k = n // 3
+    for i in range(0, n, k):
+        for j in range(0, n, k):
+            if i // k == 1 and j // k == 1: continue
+            star(k, y + i, x + j)
 
+star(N, 0, 0)
+
+ans = ""
+for line in board:
+    ans += "".join(line) + '\n'
+sys.stdout.write(ans)
