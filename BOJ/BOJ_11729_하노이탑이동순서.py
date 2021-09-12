e005e627de0b83c7e6469295114bf2868cec04a9
gwak-ongyeom <dhsrua2015@gmail.com>
2021-09-12 15:30:02 +0900

    BOJ 11729 하노이 탑 이동 순서(재귀, memoization)

--- BOJ/BOJ_11729_하노이탑이동순서.py
+++ BOJ/BOJ_11729_하노이탑이동순서.py
@@ -3,12 +3,20 @@
 import sys
 
 N = int(input())
+table = [list([""] * (4) for _ in range(4)) for _ in range(N + 1)]
 
 print(2 ** N - 1)
 def hanoi(n, fo, to, vi):
-    if n == 1: print(fo, to)
+    global ans
+    if n == 1: return f"{fo} {to}\n"
+    if table[n][fo][to]:
+        return table[n][fo][to]
     else:
-        hanoi(n - 1, fo, vi, to)
-        print(fo, to)
-        hanoi(n - 1, vi, to, fo)
-hanoi(N, 1, 3, 2)
+        tmp = ""
+        tmp += hanoi(n - 1, fo, vi, to)
+        tmp += f"{fo} {to}\n"
+        tmp += hanoi(n - 1, vi, to, fo)
+        return tmp
+        
+ans = hanoi(N, 1, 3, 2)
+sys.stdout.write(ans)
