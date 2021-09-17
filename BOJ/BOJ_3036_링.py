510c4f58f21b57fac91c15b066372ed02c52f250
AntBean94 <ssafy_coach_55@ssafy.com>
2021-09-17 10:45:57 +0900

    BOJ 3036 링(유클리드 호제법, 정수론)

---
+++ BOJ/BOJ_3036_링.py
@@ -0,0 +1,17 @@
+# BOJ 3036 링
+
+N = int(input())
+ring = list(map(int, input().split()))
+
+def gcd(n, m):
+    if m > n: n, m = m, n
+    if m == 0: return n
+    if n % m == 0: return m
+    else: return gcd(m, n % m)
+    
+
+first = ring[0]
+for n in ring[1:]:
+    # 기약분수 형태(분자, 분모를 최대공약수로 나눈 값)
+    g = gcd(first, n)
+    print(f'{first//g}/{n//g}')
