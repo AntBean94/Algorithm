62218ffe02959f4ea21a298f62a17c1c23d5aa24
gwak-ongyeom <dhsrua2015@gmail.com>
2021-10-14 22:32:13 +0900

    BOJ 9252 LCS 2(DP, 최단경로추적)

--- BOJ/BOJ_9252_LCS2.py
+++ BOJ/BOJ_9252_LCS2.py
@@ -1,14 +1,38 @@
 # BOJ 9252 LCS 2
 
-'''
+A = input()
+B = input()
 
-ACAYKP
-CAPCAK
+H = len(A)
+W = len(B)
+table = [[0] * (W + 1) for _ in range(H + 1)]
 
-ACAK
+for i in range(H):
+    for j in range(W):
+        # 같다면
+        if A[i] == B[j]:
+            table[i+1][j+1] = table[i][j] + 1
+        # 다르다면
+        else:
+            table[i+1][j+1] = max(table[i+1][j], table[i][j+1])
 
-1000 * 1000 = 1,000,000
+L = table[H][W]
+route = ""
+n = L
+y, x = H, W
+while n != 0:
+    # 위로 이동
+    while table[y][x] == n:
+        y -= 1
+    y += 1
+    # 좌로 이동
+    while table[y][x] == n:
+        x -= 1
+    x += 1
+    route += A[y-1]
+    y -= 1
+    x -= 1
+    n -= 1
 
-
-'''
-
+print(L)
+print(route[::-1])
