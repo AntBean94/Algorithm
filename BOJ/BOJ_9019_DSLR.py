4a942d430987175628c5b1138e7e1d207675bfea
gwak-ongyeom <dhsrua2015@gmail.com>
2021-10-16 03:37:47 +0900

    BOJ 9019 DSLR(BFS, 최단거리역추적)

--- BOJ/BOJ_9019_DSLR.py
+++ BOJ/BOJ_9019_DSLR.py
@@ -8,6 +8,9 @@
 DSLR에 따라 수정 BFS
 각 명령어는 큐에 함께 저장
 수정한 값 방문체크(집합 활용)
+
+(수정)
+경로 문자열 저장시 시간초과
 '''
 
 from collections import deque
@@ -19,34 +22,43 @@ def trans(x:int):
     return x
 
 def bfs(a, b):
+    vis = [0] * 10000
+    vis[a] = a
     a = trans(a)
     b = trans(b)
-    vis = set()
-    vis.add(a)
     Q = deque()
-    Q.append([a, ""])
+    Q.append(a)
     while Q:
-        a, route = Q.popleft()
-        if a == b:
-            return route
+        x = Q.popleft()
+        if x == b:
+            break
         for i in range(4):
             # D
             if i == 0:
-                new_a = trans((int(a) * 2) % 10000) 
-                new_route = route + 'D'
+                y = trans((int(x) * 2) % 10000) 
+                cmd = 'D'
             elif i == 1:
-                new_a = trans((int(a) - 1) % 10000)
-                new_route = route + 'S'
+                y = trans((int(x) - 1) % 10000)
+                cmd = 'S'
             elif i == 2:
-                new_a = a[1:] + a[0]
-                new_route = route + 'L'
+                y = x[1:] + x[0]
+                cmd = 'L'
             else:
-                new_a = a[3] + a[:3]
-                new_route = route + 'R'
-            if new_a not in vis:
-                vis.add(new_a)
-                Q.append([new_a, new_route])
-    return
+                y = x[3] + x[:3]
+                cmd = 'R'
+            y_int = int(y)
+            if not vis[y_int]:
+                vis[y_int] = (cmd, x)
+                Q.append(y)
+    # vis 배열 역추적
+    result = ""
+    tmp = b
+    while tmp != a:
+        tmp = int(tmp)
+        result += vis[tmp][0]
+        tmp = vis[tmp][1]
+
+    return result[::-1]
 
 T = int(input())
 for tc in range(T):
