8fbef7c048ad47fdd9439fff42e00b0f8c66ddd8
gwak-ongyeom <dhsrua2015@gmail.com>
2021-10-16 17:58:32 +0900

    BOJ 13913 숨바꼭질 4(BFS, 최단거리 역추적)

---
+++ BOJ/BOJ_13913_숨바꼭질4.py
@@ -0,0 +1,49 @@
+# BOJ 13913 숨바꼭질 4
+
+'''
+걷거나 순간이동
+걸을때는 -1, +1
+순간이동 *2
+
+BFS?
+'''
+from collections import deque
+
+N, K = map(int, input().split())
+
+def move(x):
+    result = set()
+    a, b, c = x - 1, x + 1, x * 2
+    if a >= 0: result.add(a)
+    if b <= 100000: result.add(b)
+    if c <= 100000: result.add(c)
+    return result
+
+def bfs(n, k):
+    vis = [0] * 100001
+    route = [0] * 100001
+    Q = deque()
+    Q.append(n)
+    vis[n] = 1
+    route[n] = n
+    while Q:
+        x = Q.popleft()
+        # print(x)
+        if x == k: break
+        for y in move(x):
+            if not vis[y]:
+                Q.append(y)
+                vis[y] = 1
+                route[y] = x
+    result = [k]
+    # print(route[:20])
+    p = k
+    while p != n:
+        # print(p)
+        p = route[p]
+        result.append(p)
+    print(len(result)-1)
+    print(*result[::-1])
+    return
+
+bfs(N, K)
