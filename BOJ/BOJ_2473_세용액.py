349649ffa4b69bba4983d152f3331ea6327c2710
gwak-ongyeom <dhsrua2015@gmail.com>
2021-10-19 15:46:41 +0900

    BOJ 2473 세 용액(투포인터)

--- BOJ/BOJ_2473_세용액.py
+++ BOJ/BOJ_2473_세용액.py
@@ -5,18 +5,19 @@
 
 산성 용액 특성값: 1 <= x <= 1,000,000,000
 알칼리성 특성값: -1,000,000,000 <= y <= -1
-
 같은 양의 세가지 용액을 혼합한 용액의 특성값은 각 특성값의 합
 
-같은 값을 중복사용하는것도 가능
-
+첫번째 방법
 두 수의 완탐 + 세번째 수 이분탐색
-O(N^2 LogN)
-
+O(N^2*LogN)
 => 시간 초과
 
+두번째 방법
+투 포인터
+A를 순회
+B, C를 결정한다.
+
 '''
-import bisect
 import sys
 input = sys.stdin.readline
 N = int(input())
@@ -24,24 +25,28 @@ arr = sorted(list(map(int, input().split())))
 
 min_result = 3000000001
 ans = []
-for i in range(N):
+for i in range(N-2):
     a = arr[i]
-    for j in range(i+1, N):
-        b = arr[j]
-        p = a + b
-        # 이분탐색으로 최적값 찾기
-        idx = bisect.bisect_left(arr[j+1:], -p)
-        if idx == N - j - 1:
-            total = p + arr[j+idx]
-        elif idx == 0:
-            total = p + arr[j+1]
+    l, r = i + 1, N - 1
+    # 투 포인터 알고리즘
+    b, c = arr[l], arr[r]
+    while l != r:
+        s = a + b + c
+        if s == 0:
+            min_result = 0
+            ans = [a, b, c]
+            break
+        elif s > 0:
+            if abs(s) < abs(min_result):
+                ans = [a, b, c]
+                min_result = s
+            r -= 1
+            c = arr[r]
         else:
-            if abs(p + arr[j + idx]) < abs(p + arr[j + idx + 1]):
-                total = p + arr[j + idx]
-            else:
-                total = p + arr[j + idx + 1]
-        if abs(total) < abs(min_result):
-            min_result = total
-            ans = [a, b, total - p]
-            if total == 0: break
+            if abs(s) < abs(min_result):
+                ans = [a, b, c]
+                min_result = s
+            l += 1
+            b = arr[l]
+    if min_result == 0: break
 print(*sorted(ans))
