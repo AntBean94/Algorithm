8017500f3a82adb54ed246c2fcdd97b3e81f6ba9
gwak-ongyeom <dhsrua2015@gmail.com>
2021-10-23 01:21:52 +0900

    BOJ 2263 트리의 순회(분할정복, 트리순회)

--- BOJ/BOJ_2263_트리의순회.py
+++ BOJ/BOJ_2263_트리의순회.py
@@ -3,11 +3,6 @@
 '''
 중위 순회, 후위 순회가 주어졌을 때 전위순회를 구하는 문제
 
-1 2 3
-1 3 2
-
-
-
 A: 4 2 5 1 3
 B: 4 5 2 3 1
 
@@ -15,24 +10,25 @@ B: 4 5 2 3 1
 2. A에서 그 값의 인덱스를 찾는다.
 - 그 값을 기준으로 좌, 우 분할
 - 각 위치의 가장 우측인덱스를 1에 전달
-
-
-
-1 2 4 5 3
-
 '''
+import sys
+sys.setrecursionlimit(150000)
 
 N = int(input())
 inorder = list(map(int, input().split()))
 postorder = list(map(int, input().split()))
+T = [0] * (N + 1)
+for i, n in enumerate(inorder): T[n] = i
 
-def find_post(l, r):
+def find_pre(il, ir, pl, pr):
+    if il == ir: return
     # 출력
-    print(postorder[l:r][-1], end=" ")
+    n = postorder[pr-1]
+    print(n, end=" ")
 
-    # 분할 정복
-    
+    # A에서 n의 인덱스 찾기
+    idx = T[n]
+    find_pre(il, idx, pl, pl + idx - il)
+    find_pre(idx + 1, ir, pl + idx - il, pr - 1)
 
-
-
-find_post(0, N)
+find_pre(0, N, 0, N)
