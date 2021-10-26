910ce0398e4e60869643f43348cbadb0ffb90e6e
AntBean94 <dhsrua2015@gmail.com>
2021-10-26 13:45:06 +0900

    BOJ 9372 상근이의 여행(MST)

---
+++ BOJ/BOJ_9372_상근이의여행.py
@@ -0,0 +1,11 @@
+# BOJ 9372 상근이의 여행
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for tc in range(T):
+    N, M = map(int, input().split())
+    for i in range(M):
+        a, b = map(int, input().split())
+    print(N - 1)
