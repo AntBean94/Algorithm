92ab86b7ebae330fc100b68bd29cfdf218ca1705
gwak-ongyeom <dhsrua2015@gmail.com>
2021-11-01 23:55:59 +0900

    BOJ 1620 나는야 포켓몬 마스터 이다솜(해시)

--- BOJ/BOJ_1620_나는야포켓몬마스터이다솜.py
+++ BOJ/BOJ_1620_나는야포켓몬마스터이다솜.py
@@ -1,6 +1,13 @@
 # BOJ 1620 나는야 포켓몬 마스터 이다솜
 
-'''
+import sys
+input = sys.stdin.readline
 
-
-'''
+N, M = map(int, input().split())
+book = {}
+for i in range(1, N + 1):
+    monster = input().rstrip()
+    book[str(i)] = monster
+    book[monster] = i
+for i in range(M):
+    print(book[input().rstrip()])
