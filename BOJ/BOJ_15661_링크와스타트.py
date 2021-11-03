a79d3a49d66247be9e9cacdb9a852d13f37a94df
gwak-ongyeom <dhsrua2015@gmail.com>
2021-11-03 23:39:25 +0900

    BOJ 15661 링크와 스타트

--- BOJ/BOJ_15661_링크와스타트.py
+++ BOJ/BOJ_15661_링크와스타트.py
@@ -1,2 +1,42 @@
 # BOJ 15661 링크와 스타트
 
+'''
+링크와 스타트
+
+N 명
+인원수는 같지 않아도 되지만 1명 이상
+Sij Sji 모두 더해야 한다.
+=> 애초에 두 점의 합으로 계산
+
+능력치 차이를 최소화
+
+완탐: 2 ^ 20 = 100만
+
+4
+0 1 2 3
+4 0 5 6
+7 1 0 2
+3 4 5 0
+
+=>
+
+4
+0 5 9 6
+0 0 6 10
+0 0 0 7
+0 0 0 0
+
+1과 2가 팀이 되면
+
+'''
+
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+member = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(1, N // 2 + 1):
+    for team_a in combinations([j for j in range(N)], i):
+        print(team_a, type(team_a))
