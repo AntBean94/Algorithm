07ae28cc3ea7b3448cf910c9b15ea46b989c60af
AntBean94 <dhsrua2015@gmail.com>
2021-11-16 10:45:47 +0900

    BOJ 1929 소수 구하기(에라토스테네스의 체)

--- BOJ/BOJ_1929_소수구하기2.py
+++ BOJ/BOJ_1929_소수구하기2.py
@@ -1,9 +1,10 @@
-# BOJ 1929 소수구하기(다시풀기)
+# BOJ 1929 소수구하기
 
 M, N = map(int, input().split())
 
 arr = [True] * (N + 1)
 for i in range(2, int(N ** 0.5) + 1):
+  if not arr[i]: continue
   for j in range(2, N//2+1):
     if i * j > N:
       break
@@ -12,4 +13,3 @@ for i in range(2, int(N ** 0.5) + 1):
 for i in range(M, len(arr)):
   if arr[i] and i > 1:
     print(i)
-
