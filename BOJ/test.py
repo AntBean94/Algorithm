d13944012a791ca2cc34693129a5a5ff0a1f99fc
gwak-ongyeom <dhsrua2015@gmail.com>
2021-11-30 23:06:51 +0900

    test파일 수정

--- BOJ/test.py
+++ BOJ/test.py
@@ -1,2 +1,20 @@
-dic = {1: 10, 2: 20}
-print(dic[2])
+import sys
+
+N = int(input())
+
+
+def solution():
+    dp = [-1] * (N + 1)
+
+    dp[0] = 0
+    dp[1] = 1
+
+    for n in range(2, N + 1):
+        dp[n] = sys.maxsize
+        for k in range(1, int(n ** 0.5) + 1):
+            dp[n] = min(dp[n], dp[n - k**2] + 1)
+
+    return dp[N]
+
+
+print(solution())
