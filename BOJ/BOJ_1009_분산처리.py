9294ff729d7c4f542ff1efbce7cbf3421a4dfaf9
gwak-ongyeom <dhsrua2015@gmail.com>
2021-12-25 19:51:44 +0900

    BOJ 1009 분산처리

--- BOJ/BOJ_1009_분산처리.py
+++ BOJ/BOJ_1009_분산처리.py
@@ -6,4 +6,6 @@ input = sys.stdin.readline
 T = int(input())
 for tc in range(T):
     a, b = map(int, input().split())
-    print(a**b % 10)
+    ans = str(pow(a, b, 10))[-1]
+    if ans == '0': print(10)
+    else: print(ans)
