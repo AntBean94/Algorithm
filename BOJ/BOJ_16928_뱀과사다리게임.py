ff19d61794bf9a78ee21ded2074db9dbf03fe076
gwak-ongyeom <dhsrua2015@gmail.com>
2021-12-28 03:45:20 +0900

    BOJ 16928 뱀과 사다리 게임(BFS)

---
+++ BOJ/BOJ_16928_뱀과사다리게임.py
@@ -0,0 +1,44 @@
+# BOJ 16928 뱀과 사다리 게임
+
+'''
+사다리와 뱀은 각각 최대 15개
+
+일종의 그래프...
+
+1번은 2, 3, 4, 5, 6, 7 로 이동가능한 그래프
+
+bfs
+'''
+
+from collections import deque
+
+N, M = map(int, input().split())
+graph = [list(range(i+1, min(i+7, 101))) for i in range(101)]
+event = {i:i for i in range(101)}
+# 사다리 정보 입력
+for i in range(N):
+    x, y = map(int, input().split())
+    event[x] = y
+# 뱀 정보 입력
+for i in range(M):
+    u, v = map(int, input().split())
+    event[u] = v
+
+def bfs(x):
+    Q = deque()
+    Q.append(x)
+    vis[x] = 1
+    while Q:
+        x = Q.popleft()
+        if x == 100: return vis[x] - 1
+        for y in graph[x]:
+            y = event[y]
+            if not vis[y]:
+                vis[y] = vis[x] + 1
+                Q.append(y)
+    return
+# print(graph)
+
+vis = [0] * 101
+print(bfs(1))
+# print(vis)
