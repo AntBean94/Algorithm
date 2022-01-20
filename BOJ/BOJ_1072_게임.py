cf92a607231ad25cbcb815aa25bc3486c655e923
gwak-ongyeom <dhsrua2015@gmail.com>
2022-01-21 01:19:41 +0900

    BOJ 1072 게임(math)

--- BOJ/BOJ_1072_게임.py
+++ BOJ/BOJ_1072_게임.py
@@ -1,7 +1,25 @@
 # BOJ 1072 게임
 
 '''
+if Z = 99면
 
+Z = int(Y / X * 100)
+Z' = (Y + K) / (X + K) * 100
+Z' = Z + 1
 
+(Z + 1)(X + K) / 100 = Y + K
+
+ZX + ZK + X + K = 100Y + 100K
+99K - ZK = ZX + X - 100Y
+K = (ZX + X - 100Y) / (99 - Z)
+K' = ceil(K)
 '''
 
+import math
+
+X, Y = map(int, input().split())
+Z = 100 * Y // X
+D = 99 - Z
+
+if D <= 0: print(-1)
+else: print(math.ceil((Z * X + X - 100 * Y) / D))
